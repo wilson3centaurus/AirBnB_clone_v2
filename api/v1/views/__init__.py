847c2ebd2282d02bd546b4e95d9fb4247b317b3a
Drihmia <drihmia.redouane@gmail.com>
2024-03-28 23:40:17 +0000

    implemented task7 : City and fixed some pycode style for app.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ from flask import Blueprint
 app_views = Blueprint('first_blueprint',
                       __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
+from api.v1.views.cities import *
