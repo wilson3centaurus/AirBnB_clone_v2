b2cd3483516a10d5a5ccef97c21522efb9ae92ee
ochijeh Franklin <ochijehfranklin@gmail.com>
2024-03-28 21:54:06 +0100

    test fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,3 +39,10 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+    def test_HBNBCommand_do_quit_docstring(self):
+	 """Test for the docstring of HBNBCommand's do_quit method"""
+	 method = HBNBCommand.do_quit
+	 self.assertIsNot(method.__doc__, None,
+		 "HBNBCommand's do_quit method needs a docstring")
+	 self.assertTrue(len(method.__doc__) >= 1,							"HBNBCommand's do_quit method needs a docstring")
