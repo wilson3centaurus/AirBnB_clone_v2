94d4c0ac5d02c60c115ac79345864af40f1e22db
Maddie <115709272+Maddily@users.noreply.github.com>
2024-03-29 15:23:44 +0200

    Remove wrong parameter

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -52,7 +52,7 @@ def delete_state(state_id):
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state(state_id):
+def create_state():
     """Creates a State object"""
 
     data = request.get_json()
