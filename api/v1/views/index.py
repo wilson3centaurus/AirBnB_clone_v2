4b199d0d8967bc8f35d1714c8dbec067628f06ea
rabiebn <rabie.bnmr@gmail.com>
2024-03-29 22:27:19 +0000

    6: Initial commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""index Module"""
+"""index View"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
---
+++ api/v1/views/states.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""State view"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methode=['GET'])
+def get_all_states():
+    """Get all states"""
+    return jsonify(storage.all(State))
+
+
+@app_views.route('/states/<state_id>', methode=['GET'])
+def get_state(state_id):
+    """Get state based on state_id"""
+    try:
+        return jsonify(storage.get(State, state_id).to_dict())
+    except Exception as e:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>', methode=['DELETE'])
+def delete_state(state_id):
+    """Delete a State"""
+    try:
+        state = storage.get(State, state_id)
+        storage.delete(state)
+        return jsonify({}), 200
+    except Exception as e:
+        abort(404)
+
+
+@app_views.route('/states', methode=['POST'])
+def post_state():
+    """Creates a State object"""
+    ## get the dict from the request {name}
+    ## check if dict is valid
+    ## return new state with 201
+    req = request.get_json()
+    if req:
+        if "name" in req.keys():
+            state = State(req["name"])
+            storage.new(state)
+            return jsonify(state), 201
+        else:
+            return "Missing name", 400
+    else:
+        return "Not a JSON", 400
+
+
+@app_views.route('/states/<state_id>', methode=['PUT'])
+def put_state(state_id):
+    """Updates a State object"""
+    try:
+        state = storage.get(State, state_id)
+    except Exception as e:
+        abort(404)
+    req = request.get_json()
+    if req:
+        state.name = req["name"]
+        ## I might need to state.save() here
+        return jsonify(state), 200
+    else:
+        return "Not a JSON", 400
+
+
+if __name__ == '__main__':
+    pass
