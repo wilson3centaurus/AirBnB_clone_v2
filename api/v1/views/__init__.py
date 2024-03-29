5e9115c6d60ef6a4e82c15b4f5b8bb0ddefcd498
RangiraDave <rangiradev666@gmail.com>
2024-03-29 17:26:31 +0200

    Removed shebang

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,21 @@
-#!/usr/bin/python3
-""" init file for views blueprint. """
+# #!/usr/bin/python3
+# """ init file for views blueprint. """
 
-# from flask import Flask
 from flask import Blueprint
-# from api.v1.views.index import *
-
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
