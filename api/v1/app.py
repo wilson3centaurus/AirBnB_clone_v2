65e9acdb67161c38407393dfbc624dbccb4e34fc
Sibongile <sibongile.nhlema@gmail.com>
2024-03-29 10:01:29 +0200

    added debug=TRue statement for jsonify to work correctly

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from models import storage
 
@@ -16,7 +16,13 @@ def teardown_storage(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    """ Return a JSON response with status code 404 """
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
