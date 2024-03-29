7545f2d71acc9a9ec6390e42e9f6f9a110c0235f
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-29 13:37:09 -0700

    Update DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,6 +71,17 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        if cls is None:
+            count = sum([self.__session.query(cls).count()
+                        for cls in classes.values()])
+        elif cls in classes.values():
+            count = self.__session.query(cls).count()
+        else:
+            count = 0
+        return count
+
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
