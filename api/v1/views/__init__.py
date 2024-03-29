7b06941f1238cc43e2fb2f87d3f2e3dc80ac14ad
RangiraDave <rangiradev666@gmail.com>
2024-03-29 12:58:06 +0200

    corrected init in views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ init file for views blueprint. """
 
-from flask import Flask
+# from flask import Flask
 from flask import Blueprint
+# from api.v1.views.index import *
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from models import storage
 from api.v1.views.index import *
-
-
-app_views = Blueprint('app_views', url_prefix='/api/v1', __name__)
