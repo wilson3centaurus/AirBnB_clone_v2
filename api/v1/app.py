23d39a12f389b06d4731d9ec54ff1c55893a1f9c
rabiebn <rabie.bnmr@gmail.com>
2024-03-29 21:36:46 +0000

    5: 404 handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask app Module"""
-from flask import Flask
+from flask import Flask, jsonify, abort
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 def teardown_db(error):
     """close storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
