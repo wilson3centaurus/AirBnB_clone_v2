a305763105a8dc9044fae3ccab6472e1af756d6f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-03-29 14:08:46 +0200

    Import states module

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """ Creating a flask Blueprint named app_views """
 
 from flask import Blueprint
+from api.v1.views.index import *
+from api.v1.views import states
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
