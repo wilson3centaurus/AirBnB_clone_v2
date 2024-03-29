6161946adae1f6b0b8de2f2bb7a029ddf6ac5c28
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-29 17:55:22 +0200

    update for 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,7 +18,7 @@ def teardown(self):
 @app.errorhandler(404)
 def not_found(e):
     """Not found error handler"""
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", default='0:0:0:0')
