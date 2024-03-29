40c2e681b2321ddf6471ad1080f3b24816c2c696
Maddie <115709272+Maddily@users.noreply.github.com>
2024-03-29 07:45:17 +0200

    Enable getting and counting objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieves an object"""
+
+        obj = [value for value in self.all(cls).values()
+               if cls in classes.values() and id and value.id == id]
+        if len(obj) == 0:
+            return None
+        return obj[0]
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching
+        the given class. If no class is passed, it returns
+        the count of all objects in storage"""
+
+        objs = self.all(cls)
+        return len(objs)
