fad80a5659ad010a542f920d4348e27562304ec2
Sibongile <sibongile.nhlema@gmail.com>
2024-03-29 19:44:12 +0200

    fixed the path- keeping Blueprint in mind

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -15,9 +15,9 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/api/v1/states/<state_id>/cities',
+@app_views.route('/states/<state_id>/cities',
                  methods=['GET'], strict_slashes=False)
-def get_cities_by_state(state_id):
+def get_cities_of_a_state(state_id):
     """
     Retrieves the list of all City objects of a State
     Parameters:
@@ -33,9 +33,9 @@ def get_cities_by_state(state_id):
     return jsonify(cities)
 
 
-@app_views.route('/api/v1/cities/<city_id>',
+@app_views.route('/cities/<city_id>',
                  methods=['GET'], strict_slashes=False)
-def get_city(city_id):
+def get_a_city_using_id(city_id):
     """
     Retrieves a city object
     Parameters:
@@ -48,9 +48,9 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
-@app_views.route('/api/v1/cities/<city_id>',
+@app_views.route('/cities/<city_id>',
                  methods=['DELETE'], strict_slashes=False)
-def delete_city(city_id):
+def delete_a_city(city_id):
     """ Deletes a City object """
     city = storage.get(City, city_id)
     if city is None:
@@ -62,9 +62,9 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/api/v1/states/<state_id>/cities',
+@app_views.route('/states/<state_id>/cities',
                  methods=['POST'], strict_slashes=False)
-def create_city(state_id):
+def create_a_city(state_id):
     """ Creates a City """
     state = storage.get(State, state_id)
     if state is None:
@@ -84,7 +84,7 @@ def create_city(state_id):
 
 @app_views.route('/cities/<city_id>',
                  methods=['PUT'], strict_slashes=False)
-def update_city(city_id):
+def update_a_city(city_id):
     """Updates a City object"""
     city = storage.get(City, city_id)
     if city is None:
