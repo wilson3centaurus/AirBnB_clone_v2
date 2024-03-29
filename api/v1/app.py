0dfb398675694da037b30a176e475006dd6c44f0
rabiebn <rabie.bnmr@gmail.com>
2024-03-29 20:59:30 +0000

    3: modified HOST&PORT

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,4 +16,9 @@ def teardown_db(error):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    HBNB_API_HOST = getenv("HBNB_API_HOST")\
+        if getenv("HBNB_API_HOST") else "0.0.0.0"
+    HBNB_API_PORT = int(getenv("HBNB_API_PORT"))\
+        if getenv("HBNB_API_PORT") else 5000
+
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
