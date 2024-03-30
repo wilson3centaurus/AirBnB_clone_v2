a0106f6c371af60e4c1fd3e89fd54f718da8ba3a
RadouaneAbn <radouaneabn@gmail.com>
2024-03-30 01:29:49 +0000

    Task 2 update DBstorage and Filestorage:     - add a get() function that returns as instance with given id.     - add a count() function that returns the number of all instances       or the number all instances of given class.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,6 +48,19 @@ class FileStorage:
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
+    def get(self, cls, id):
+        """ This function return a sepecific object with a given id """
+        for obj in self.all(cls).values():
+            if obj.id == id:
+                return obj
+        return None
+    
+    def count(self, cls=None):
+        """ This function return the number of instances of a class
+            or the number of all instances
+        """
+        return len(self.all(cls))
+
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
