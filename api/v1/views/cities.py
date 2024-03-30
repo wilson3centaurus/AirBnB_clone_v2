18a4dee5f2282d6d609e1a7d0f503ea0c998d3dc
millimesi <millionmesi1@gmail.com>
2024-03-30 14:13:58 +0300

    put modified

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -70,56 +70,9 @@ def put_city(city_id=None):
     req_json = request.get_json()
     if req_json is None:
         abort(400, 'Not a JSON')
-    city.cls_update(req_json)
+    for key, value in req_json.items():
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+            setattr(city, key, value)
+
+    storage.save()
     return jsonify(city.to_dict()), 200
-
-
-# @app_views.route('/states/<state_id>', methods=['DELETE'])
-# def delete_state(state_id):
-#     """Delete state object based on ID"""
-#     state = storage.get(State, state_id)
-#     if state:
-#         storage.delete(state)
-#         storage.save()
-#         return jsonify({}), 200
-#     else:
-#         return jsonify({'error': 'Not found'}), 404
-
-
-# @app_views.route('/states/', methods=['POST'])
-# def create_state():
-#     """Creates new state"""
-#     data = request.get_json()
-#     if data is None:
-#         return jsonify({'error': 'Not a JSON'}), 400
-#     if 'name' not in data:
-#         return jsonify({'error': 'Missing name'}), 400
-
-#     new_state = State(**data)
-#     new_state.save()
-
-#     return jsonify(new_state.to_dict()), 201
-
-
-# @app_views.route('/states/<state_id>', methods=['PUT'])
-# def update_state(state_id):
-#     """Update a State"""
-#     state = storage.get(State, state_id)
-#     if not state:
-#         return jsonify({'error': 'Not found'}), 404
-
-#     data = request.get_json()
-#     if data is None:
-#         return jsonify({'error': 'Not a JSON'}), 400
-
-#     # Remove keys to be ignored
-#     data.pop('id', None)
-#     data.pop('created_at', None)
-#     data.pop('updated_at', None)
-
-#     # Update state object with new data
-#     for key, value in data.items():
-#         setattr(state, key, value)
-
-#     state.save()
-#     return jsonify(state.to_dict()), 200
