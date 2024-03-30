fd1e3ea06e7cf7aecec14db2a78c50c17591d391
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-03-30 21:13:02 +0000

    1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,7 +69,7 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
-def get(self,cls, id):
+    def get(self, cls, id):
         """get method"""
         all_class = self.all(cls)
 
