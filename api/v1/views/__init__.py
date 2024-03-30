d39ae2069b6e915f21650653d9e6efb790179120
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-03-30 21:00:37 +0300

    AirBnB

--- api/v1/views/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Creating a Flask app"""
-from flask import Blueprint
-
-# Create a Blueprint instance
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
-
-from api.v1.views.index import *
