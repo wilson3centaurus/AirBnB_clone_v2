2aa60c4d878f75c6a22368de20c3b538bbd1942e
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-30 11:02:40 -0700

    Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Creates Flask app
+"""
+
+from os import getenv
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', '5000'))
+    app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates flask app"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def api_status():
+    """
+
+    """
+    response = {'status': "OK"}
+    return jsonify(response)
