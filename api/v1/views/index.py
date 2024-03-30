1d1cc8a58c3e4bf42265aa4b6931d82e91067f94
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2024-03-30 14:15:22 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Index """
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
