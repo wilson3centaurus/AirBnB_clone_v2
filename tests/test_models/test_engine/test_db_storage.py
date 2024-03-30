e9d79168439d54fde2071d3b6b90637b6a22eae4
oumaima <oumaimanaanaapro@gmail.com>
2024-03-30 01:30:09 +0000

    task2

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -68,21 +68,49 @@ test_db_storage.py'])
                             "{:s} method needs a docstring".format(func[0]))
 
 
-class TestFileStorage(unittest.TestCase):
+class TestDBStorage(unittest.TestCase):
     """Test the FileStorage class"""
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t != 'fs', "not testing file storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
         self.assertIs(type(models.storage.all()), dict)
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t != 'fs', "not testing file storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t != 'fs', "not testing file storage")
     def test_new(self):
         """test that new adds an object to the database"""
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t != 'fs', "not testing file storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'fs', "not testing file storage")
+    def test_get(self):
+        """ Test that gets the object by id"""
+
+        storage = FileStorage()
+        all_objects = storage.all()
+        first_obj = list(storage.all(State).values())[0]
+        first_obj_id = first_obj.id
+        first_obj_cls = first_obj.__class__.__name__
+
+        obj_get = storage.get(first_obj_cls, str(first_obj_id))
+
+        self.assertEqual(obj_get, first_obj)
+
+    @unittest.skipIf(models.storage_t != 'fs', "not testing file storage")
+    def test_count(self):
+        """ Test that count method is working properly """
+        storage = FileStorage()
+
+        all_objects = storage.all()
+        count_all = len(all_objects)
+
+        state_objects = storage.all(State)
+        count_states = len(state_objects)
+
+        self.assertEqual(count_all, storage.count())
+        self.assertEqual(count_states, storage.count(State))
