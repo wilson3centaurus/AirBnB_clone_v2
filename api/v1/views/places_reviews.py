44ab329431980c5ac28433e55da3cb62aeaf56fa
millimesi <millionmesi1@gmail.com>
2024-03-30 17:59:11 +0300

    task12 and slash_strict false for the global

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -27,7 +27,7 @@ def get_reviews(review_id=None):
     review = storage.get(Review, review_id)
     if review is None:
         abort(404)
-     return jsonify(review.to_dict())
+    return jsonify(review.to_dict())
 
 
 @app_views.route('/eviews/<review_id>', methods=['DELETE'],
@@ -52,52 +52,3 @@ def post_reviews_by_places(place_id=None):
         abort(400, 'Not a JSON')
     if "user_id" not in req_json:
         abort(400, 'Missing user_id')
-
-"""
-@app_views.route('/cities/<city_id>/places', methods=['POST'],
-                 strict_slashes=False)
-def post_cities_by_places(city_id=None):
-    # Create a new Place associated with a City
-    req_json = request.get_json()
-    if req_json is None:
-        abort(400, 'Not a JSON')
-    if "user_id" not in req_json:
-        abort(400, 'Missing user_id')
-    user_id = req_json["user_id"]
-    user = storage.get(User, user_id)
-    if user is None:
-        abort(404)
-
-    if "name" not in req_json:
-        abort(400, 'Missing name')
-
-    req_json['city_id'] = city_id
-    new_place = Place(**req_json)
-    new_place.save()
-
-    return jsonify(new_place.to_dict()), 201
-
-
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def put_place(place_id):
-    # Update a Place object by ID
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-
-    req_json = request.get_json()
-    if req_json is None:
-        abort(400, 'Not a JSON')
-
-    # Ignore keys: id, user_id, city_id, created_at, updated_at
-    keys_to_ignore = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
-    for key in keys_to_ignore:
-        req_json.pop(key, None)
-
-    # Update Place object with new data
-    for key, value in req_json.items():
-        setattr(place, key, value)
-
-    storage.save()
-
-    return jsonify(place.to_dict()), 200"""
