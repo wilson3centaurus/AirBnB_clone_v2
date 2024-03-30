bcbc053474057529e28a436c469b0e6a0927bb56
Samuel-seneni <samuelmunialo2005@gmail.com>
2024-03-30 13:27:19 -0700

    Improve storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -69,7 +69,7 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
-      def get(self, cls, id):
+    def get(self, cls, id):
         """
         gets an object
         Args:
