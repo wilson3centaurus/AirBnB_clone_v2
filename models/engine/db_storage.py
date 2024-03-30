fa5f0a7bc8e6e37a82bb20caf16825a7e1918419
Zeezbaba <Azeez_okeleji>
2024-03-30 16:01:44 -0700

    Updating Test files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """method to retrieve one object"""
+        if cls in classes.values() and id and type(id) == str:
+            objs = self.all(cls)
+            for key, value in objs.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        obj_num = self.all(cls)
+        if cls in classes.values():
+            obj_num = self.all(cls)
+        return len(obj_num)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """method to retrieve one object"""
+        if cls in classes.values() and id and type(id) == str:
+            objs = self.all(cls)
+            for key, value in objs.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        obj_num = self.all(cls)
+        if cls in classes.values():
+            obj_num = self.all(cls)
+        return len(obj_num)
