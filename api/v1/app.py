7588e49037d15e7876b5f0cd15e2dbb119c0e521
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-30 23:58:32 +0000

    STARTING THE API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python
 """ main app """
+from flask import Flask, jsonify
+from api.v1.views import app_views
+import os
+
+
+app = Flask('__name__')
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close(exception=None):
+    from models import storage
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python
 """ Basic routes """
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def count():
+    """
+        stats of all objects route
+        :return: A json representation of all objs
+    """
+    from models import storage
+    result = {}
+    classes = {"Amenity": "amenities",
+               "City": "cities",
+               "Place": "places",
+               "Review": "reviews",
+               "State": "states",
+               "User": "users"
+               }
+
+    for cls, lab in classes.items():
+        result.update({lab: storage.count(cls)})
+
+    return jsonify(result)
