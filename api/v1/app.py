ab5cfe20667901ffa1b3789e813e6a9b5b51a179
sara <saramustafa1842002@gmail.com>
2024-03-30 20:58:57 +0200

    finished

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,13 +25,15 @@ def tear(self):
 def not_found(error):
     ''' handles 404 error and gives json formatted response '''
     return make_response(jsonify({'error': 'Not found'}), 404)
-    if __name__ == '__main__':
-        if getenv("HBNB_API_HOST") is None:
-            HBNB_API_HOST = '0.0.0.0'
-        else:
-            HBNB_API_HOST = getenv("HBNB_API_HOST")
-        if getenv("HBNB_API_PORT") is None:
-            HBNB_API_PORT = 5000
-        else:
-            HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
-        app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+
+
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
