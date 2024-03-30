2e597851187f0125395b7945bd7cc50a48451abb
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-30 12:10:00 -0700

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Creates Flask app
 """
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -12,7 +12,23 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
+@teardown_appcontext
+def teardown_engine(exception):
+
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handle 404 errors
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
+
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', '5000'))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
