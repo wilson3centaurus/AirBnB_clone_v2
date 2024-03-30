c52d9e91c171cad76a729c73b17ecbc7b3bd08b7
AxiomDays <tobeoke@gmail.com>
2024-03-31 00:10:23 +0100

    count api implementation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,13 @@
 """index module"""
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
@@ -13,3 +19,14 @@ def status():
     response = jsonify(status)
     response.status_code = 200
     return response
+
+@app_views.route('/stats')
+def stats():
+    """return number of each class"""
+    return jsonify({
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)})
