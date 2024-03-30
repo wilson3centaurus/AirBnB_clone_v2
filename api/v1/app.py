d29a01145c64e96ea55d731b54d9b6e1cf695b16
adiomi90 <adiomi90@outlook.com>
2024-03-30 07:44:53 -0700

    jsonify import

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Create Flask app, and register the blueprint for app_views
 """
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -18,6 +18,7 @@ app.register_blueprint(app_views)
 def teardown(e):
     """close any current active route"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
