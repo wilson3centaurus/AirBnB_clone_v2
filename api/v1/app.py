bd3dd4c15370ce970a3a95d546520ec4df0ff54f
Love <mzeelovegeneral@gmail.com>
2024-03-30 21:03:16 +0100

    feat(api): design json response for bad request

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """"app module"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,13 @@ def close_storage(exception):
     """close storage"""
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """return 404"""
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 if __name__ == "__main__":
     app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
