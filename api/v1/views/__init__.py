6ddbc28be31e625db66ba7e167cfbee957dd7434
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-30 11:10:57 -0700

    Status of your API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Creates flask app blueprint"""
 
+from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
