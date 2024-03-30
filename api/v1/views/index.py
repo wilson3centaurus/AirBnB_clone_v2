1c0c580c41dd873a912a1fc70582ff05749cb7cc
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-03-30 10:25:33 -0700

    fix index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,23 +3,11 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
-@app_views.route("/status", methods=["GET"], strict_slashes=False)
-def status_ok():
-    """Returns Status OK"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route("/stats", methods=["GET"], strict_slashes=False)
-def count_obj():
-    """Counts Objects per Type"""
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieve the number of each object type"""
     counts = {
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
