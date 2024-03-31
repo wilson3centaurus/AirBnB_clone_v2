9b5be6b81a7a25a521819d3cf19114c891939731
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-03-31 17:30:00 +0200

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -25,9 +25,8 @@ class test_console(unittest.TestCase):
         sys.stdout = self.capt_out
 
     def tearDown(self):
-    """ Restore the original sys.stdout after each test. """
-    sys.stdout = self.backup
-
+        ''''''
+        sys.stdout = self.backup
 
     def create(self):
         ''' create an instance of the HBNBCommand class'''
@@ -39,67 +38,71 @@ class test_console(unittest.TestCase):
         self.assertTrue(console.onecmd("quit"))
 
     def test_EOF(self):
-    """ Test if the console handles the EOF (End-of-File) command correctly. """
-    console = self.create()
-    self.assertTrue(console.onecmd("EOF"))
-
+        ''' Test EOF exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("EOF"))
 
     def test_all(self):
-    """ Test if the output of the 'all' command is of type string. """
-    console = self.create()
-    console.onecmd("all")
-    self.assertTrue(isinstance(self.capt_out.getvalue(), str))
-
+        ''' Test all exists'''
+        console = self.create()
+        console.onecmd("all")
+        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
 
     @unittest.skipIf(db == "db", "Testing database storage only")
     def test_show(self):
-    """ Test if the output of 'show' command is of type string. """
-    console = self.create()
-    console.onecmd("create User")
-    user_id = self.capt_out.getvalue()
-    sys.stdout = self.backup
-    self.capt_out.close()
-    self.capt_out = StringIO()
-    sys.stdout = self.capt_out
-    console.onecmd("show User " + user_id)
-    x = (self.capt_out.getvalue())
-    sys.stdout = self.backup
-    self.assertTrue(str is type(x))
-
-
-   @unittest.skipIf(db == "db", "Testing database storage only")
-    def test_show_class_name(self):
-    """ Test the error message when the class name is missing for show command. """
-    console = self.create()
-    console.onecmd("create User")
-    user_id = self.capt_out.getvalue()
-    sys.stdout = self.backup
-    self.capt_out.close()
-    self.capt_out = StringIO()
-    sys.stdout = self.capt_out
-    console.onecmd("show")
-    x = (self.capt_out.getvalue())
-    sys.stdout = self.backup
-    self.assertEqual("** class name missing **\n", x)
-
-
-    def test_show_class_name(self):
-    """ Test the show message error for missing instance id. """
-    console = self.create()
-    console.onecmd("create User")
-    user_id = self.capt_out.getvalue()
-    sys.stdout = self.backup
-    self.capt_out.close()
-    self.capt_out = StringIO()
-    sys.stdout = self.capt_out
-    console.onecmd("show User")
-    x = (self.capt_out.getvalue())
-    sys.stdout = self.backup
-    self.assertEqual("** instance id missing **\n", x)
+        '''
+            Testing that show exists
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User " + user_id)
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertTrue(str is type(x))
 
     @unittest.skipIf(db == "db", "Testing database storage only")
-def test_show_no_instance_found(self):
-        """ Test if an appropriate error message is shown when no instance is found. """
+    def test_show_class_name(self):
+        '''
+            Testing the error messages for class name missing.
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show")
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertEqual("** class name missing **\n", x)
+
+    def test_show_class_name(self):
+        '''
+            Test show message error for id missing
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User")
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertEqual("** instance id missing **\n", x)
+
+    @unittest.skipIf(db == "db", "Testing database storage only")
+    def test_show_no_instance_found(self):
+        '''
+            Test show message error for id missing
+        '''
         console = self.create()
         console.onecmd("create User")
         user_id = self.capt_out.getvalue()
@@ -113,24 +116,20 @@ def test_show_no_instance_found(self):
         self.assertEqual("** no instance found **\n", x)
 
     def test_create(self):
-        """ Test creating an object using the create method. """
+        """ Test that create works """
         console = self.create()
         console.onecmd("create User email=adriel@hbnb.com password=abc")
         self.assertTrue(isinstance(self.capt_out.getvalue(), str))
 
     def test_class_name(self):
-        """ Test if an appropriate error message is shown when the class name is missing. """
+        """ Testing the error messages for class name missing. """
         console = self.create()
         console.onecmd("create")
         x = (self.capt_out.getvalue())
         self.assertEqual("** class name missing **\n", x)
 
-
-class TestClassNameDoesNotExist(unittest.TestCase):
-    """ Test case for ensuring error message when class doesn't exist. """
-
-    def test_class_name_does_not_exist(self):
-        """ Test if an appropriate error message is shown when the class doesn't exist. """
+    def test_class_name_doest_exist(self):
+            """Test error messages for class name missing."""
         console = self.create()
         console.onecmd("create Binita")
         x = (self.capt_out.getvalue())
@@ -138,9 +137,8 @@ class TestClassNameDoesNotExist(unittest.TestCase):
 
     @unittest.skipIf(db != 'db', "Testing DBstorage only")
     def test_create_db(self):
-        """Test creating an object in the database storage."""
+        """ Test creating an object in the database storage."""
         console = self.create()
         console.onecmd("create State name=California")
         result = storage.all("State")
         self.assertTrue(len(result) > 0)
-
