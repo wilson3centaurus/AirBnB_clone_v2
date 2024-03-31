9be61a01bb18f1c78433a360b742f45478ecc1b0
RadouaneAbn <radouaneabn@gmail.com>
2024-03-31 23:29:41 +0000

    try to find the problem (remove threded)

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,8 +4,7 @@ view for State objects that handles
 all default RESTFul API actions
 """
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
-from werkzeug.exceptions import BadRequest
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
