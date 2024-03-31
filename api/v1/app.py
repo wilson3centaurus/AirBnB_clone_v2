68ec9f172d46bb851ea8a2bcc4177320595ae953
OjElsa <elsahojwang06@gmail.com>
2024-03-31 14:09:18 -0700

    status of the API

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""first endpoint APIs"""
+
+import os
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+@app_errorhandler(404)
+def not_found(error):
+    """handle 404 error if found"""
+    return jsonify({"error": "Not Found"}), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Blueprints intialization of APIs"""
+
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+
+# wildcard importing of everything in the api.v1.views.index package
+from api.v1.views.index import *
