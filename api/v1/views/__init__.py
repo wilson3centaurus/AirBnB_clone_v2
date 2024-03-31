3dcba516681d32240d35e49a05bdbaddc8d5ab31
Salome-N <njengasalome87@gmail.com>
2024-03-31 15:27:18 -0700

    Update for Stae

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,5 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # Wildcard import of everything in the package api.v1.views.index
 from api.v1.views.index import *
+from api.v1.views.states import *
 from api.v1.views.cities import *
