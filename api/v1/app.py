e8022ee2297ab4a92f201896fa52118fa67d4b0c
aelbouaa <hbouaami@gmail.com>
2024-03-31 05:49:10 +0000

    fixing lines tabs

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ API
 """
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
@@ -38,10 +39,10 @@ def not_found(error):
 
 if __name__ == "__main__":
     if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST")
-    port = getenv("HBNB_API_PORT")
-    if host is None:
-        host = '0.0.0.0'
-    if port is None:
-        port = '5000'
-    app.run(host=host, port=port)
+        host = getenv("HBNB_API_HOST")
+        port = getenv("HBNB_API_PORT")
+        if host is None:
+            host = '0.0.0.0'
+        if port is None:
+            port = '5000'
+        app.run(host=host, port=port)
