76e5cce501f4348674bbd589581af464af0588e0
AlexDelei <alexdelei125@gmail.com>
2024-03-31 14:58:37 +0000

    pep 8 issues

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def tear_down_db(exception):
     """performing a cleanup task"""
@@ -20,5 +21,4 @@ def tear_down_db(exception):
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(getenv("HBNB_API_PORT", 5000))
-
     app.run(host=host, port=port, threaded=True)
