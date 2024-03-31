12e4b4f886e8a954845b28c5205ae07a15768bb3
rabiebn <rabie.bnmr@gmail.com>
2024-03-31 22:23:07 +0000

    12: added origins opt to cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,8 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, ressources={"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, ressources={r"/api/v1/*": {"origins": ["0.0.0.0"]}},
+            origins="0.0.0.0")
 
 
 @app.teardown_appcontext
