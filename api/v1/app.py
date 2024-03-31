01b5fd911f63ad7e1c54c05504979f221af9542c
rabiebn <rabie.bnmr@gmail.com>
2024-03-31 21:59:38 +0000

    12: implimenting CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Flask app Module"""
 from flask import Flask, jsonify, abort
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, ressources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
