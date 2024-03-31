a16c9d63f3a2d2a03877d06cea10ea26803b34b7
RadouaneAbn <radouaneabn@gmail.com>
2024-03-31 23:35:54 +0000

    handle more exeptions

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -74,9 +74,11 @@ def create_city(state_id):
     if state is None:
         abort(404)
     try:
-        request_data = request.get_json()
-    except BadRequest:
-        abort(400, description="Not a JSON")
+        if not request.get_json():
+            abort(400, "Not a JSON")
+    except Exception:
+        abort(400, "Not a JSON")
+    request_data = request.get_json()
 
     if 'name' not in request_data:
         abort(400, description="Missing name")
@@ -98,10 +100,11 @@ def update_city(city_id):
     if not city:
         abort(404)
     try:
-        request_data = request.get_json()
-    except BadRequest:
-        abort(400, description="Not a JSON")
-
+        if not request.get_json():
+            abort(400, "Not a JSON")
+    except Exception:
+        abort(400, "Not a JSON")
+    request_data = request.get_json()
     for key, value in request_data.items():
         if key not in ('id', 'created_at', 'updated_at', 'state_id'):
             setattr(city, key, value)
