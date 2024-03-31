86d1be62283dcfdac7303f45f85493fc409e9d56
bini34 <biniyamambachew@gmail.com>
2024-03-31 16:42:11 +0300

    add task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -10,6 +10,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_storage(exception):
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
