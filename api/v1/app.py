0f6e67af7702a77ed7684afc574f203d35e7778c
bini34 <biniyamambachew@gmail.com>
2024-03-31 17:50:23 +0300

    add task 11 and 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 @app.teardown_appcontext
 def close_storage(exception):
     storage.close()
