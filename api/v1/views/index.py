3b646526f41ae7610f5e5e373d360b9091d2c423
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-03-31 10:09:22 -0700

    Task 4 : fix index code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """index View"""
 from flask import jsonify
 from api.v1.views import app_views
+import models
 from models import storage
 from models.amenity import Amenity
 from models.city import City
@@ -11,13 +12,13 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route("/status", methods=["GET"], strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status_ok():
     """Returns Status OK"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+@app_views.route("/stats", strict_slashes=False)
 def count_obj():
     """Counts Objects per Type"""
     counts = {
