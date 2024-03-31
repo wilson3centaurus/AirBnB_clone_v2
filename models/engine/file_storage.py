8080414cddd6563134754c7f59c60505e2e65400
DevBasem <engr.basemahmed@gmail.com>
2024-03-30 21:07:13 -0700

    Implement get and count methods in DBStorage and FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):  # sourcery skip: use-next
+        """"A method to retrieve one object"""
+        all_object = models.storage.all(cls)
+        for obj in all_object:
+            if all_object[obj].id == id:
+                return all_object[obj]
+        return None
+
+    def count(self, cls=None):
+        """method to count"""
+        if cls is None:
+            return len(models.storage.all())
+        else:
+            return len(models.storage.all(cls))
