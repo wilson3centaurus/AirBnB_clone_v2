45708bda7d58c419f8ddd85863f60949e6024dd4
RadouaneAbn <radouaneabn@gmail.com>
2024-03-31 02:58:33 +0000

    debug 2

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views, app_states, app_cities, app_amenities
-from api.v1.views import app_users, app_places, app_places_reviews
 from flask import Flask, make_response, jsonify
+from api.v1.views import app_users, app_places, app_places_reviews
 from models import storage
 
 HBNB_API_HOST = '0.0.0.0'
@@ -12,32 +12,32 @@ app = Flask(__name__)
 """<----------------- view routes ----------------->"""
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
-# """<---------------- states routes ---------------->"""
-# app.register_blueprint(app_states, url_prefix="/api/v1/states")
+"""<---------------- states routes ---------------->"""
+app.register_blueprint(app_states, url_prefix="/api/v1/states")
 
-# """<---------------- cities routes ---------------->"""
-# app.register_blueprint(app_cities, url_prefix="/api/v1/states",
-#                        name="state->cities")
-# app.register_blueprint(app_cities, url_prefix="/api/v1/cities",
-#                        name="cities")
+"""<---------------- cities routes ---------------->"""
+app.register_blueprint(app_cities, url_prefix="/api/v1/states",
+                       name="state->cities")
+app.register_blueprint(app_cities, url_prefix="/api/v1/cities",
+                       name="cities")
 
-# """<--------------- amenities routes -------------->"""
-# app.register_blueprint(app_amenities, url_prefix="/api/v1/amenities")
+"""<--------------- amenities routes -------------->"""
+app.register_blueprint(app_amenities, url_prefix="/api/v1/amenities")
 
-# """<----------------- users routes ---------------->"""
-# app.register_blueprint(app_users, url_prefix="/api/v1/users")
+"""<----------------- users routes ---------------->"""
+app.register_blueprint(app_users, url_prefix="/api/v1/users")
 
-# """<---------------- places routes ---------------->"""
-# app.register_blueprint(app_places, url_prefix="/api/v1/cities",
-#                        name="city->places")
-# app.register_blueprint(app_places, url_prefix="/api/v1/places",
-#                        name="places")
+"""<---------------- places routes ---------------->"""
+app.register_blueprint(app_places, url_prefix="/api/v1/cities",
+                       name="city->places")
+app.register_blueprint(app_places, url_prefix="/api/v1/places",
+                       name="places")
 
-# """<---------------- reviews routes --------------->"""
-# app.register_blueprint(app_places_reviews, url_prefix="/api/v1/places",
-#                        name="place->reviews")
-# app.register_blueprint(app_places_reviews, url_prefix="/api/v1/reviews",
-#                        name="reviews")
+"""<---------------- reviews routes --------------->"""
+app.register_blueprint(app_places_reviews, url_prefix="/api/v1/places",
+                       name="place->reviews")
+app.register_blueprint(app_places_reviews, url_prefix="/api/v1/reviews",
+                       name="reviews")
 
 
 @app.teardown_appcontext
@@ -65,5 +65,4 @@ def not_found(error):
 if __name__ == "__main__":
     app.run(host=HBNB_API_HOST,
             port=HBNB_API_PORT,
-            threaded=True,
-            debug=True)
+            threaded=True)
