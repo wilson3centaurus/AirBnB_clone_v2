5b9a6230ef007091b405f4c44dbacca647955710
nadashaban10 <nadashaban10@github.com>
2024-03-31 07:26:53 +0200

    pull req

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+import models
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -55,7 +56,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +69,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):  # sourcery skip: use-next
+        """"A method to retrieve one object"""
+        all_object = models.storage.all(cls)
+        for obj in all_object:
+            if all_object[obj].id == id:
+                return all_object[obj]
+        return None
+
+    def count(self, cls=None):
+        """method to count"""
+        if cls is None:
+            return len(models.storage.all())
+        else:
+            return len(models.storage.all(cls))
