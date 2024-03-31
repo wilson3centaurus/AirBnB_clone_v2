f29b7394b9121ec565235a8a45fedadd956cea13
Tashydaije <mbuguanatasha01@gmail.com>
2024-03-31 06:45:50 -0700

    API blueprint initialization

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""API blueprint initialization"""
+
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# Wildcard import of everything in the package api.v1.views.index
+from api.v1.views.index import *
