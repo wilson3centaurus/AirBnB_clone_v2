fc8f525c0c12012e2980aa45ce9b8e3a861894fa
OjElsa <elsahojwang06@gmail.com>
2024-03-31 16:34:52 -0700

    State APIs

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # wildcard importing of everything in the api.v1.views.index package
 from api.v1.views.index import *
+from api.v1.views.states import *
