3034fc212c5e55ce6e68bf1b58e8d4d51a92d901
Adejare <adejare.arasheed@gmail.com>
2024-03-31 23:01:03 +0100

    RESTful API

---
+++ api/v1/views/cities.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+create a new view for City objects that handles all default RESTful API
+actions
+"""
+from models import storage
+from api.v1.views import app_views
+from flask import make_response, jsonify, abort, request
+from models.city import City
+
+
+@app_views.before_request
+def new_storage_values():
+    """Called before any request is made to refresh storage"""
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'],
+                 strict_slashes=False)
+def all_cities(state_id, cities=None):
+    """list of all City objects of a State"""
+    state_inst = storage.get('State', state_id)
+    if not state_inst:
+        abort(404)
+    if request.method == 'GET':
+        city_instances = [city.to_dict() for city in state_inst.cities]
+        return make_response(jsonify(city_instances), 200)
+
+    elif request.method == 'POST':
+        if not request.is_json or not request.get_json().get('name', None):
+            abort(400)
+        data = request.get_json()
+        data['state_id'] = state_id
+        new_inst = City(**data)
+        new_inst.save()
+        data = storage.get(City, new_inst.id).to_dict()
+        return make_response(jsonify(data), 201)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def city_obj(city_id):
+    """Retrieves a City Object"""
+    city_inst = storage.get(City, city_id)
+
+    if not city_inst:
+        abort(404)
+
+    if request.method == 'GET':
+        return make_response(jsonify(city_inst.to_dict()), 200)
+
+    elif request.method == 'DELETE':
+        storage.delete(city_inst)
+        storage.save()
+        return jsonify({}), 200
+
+    elif request.method == 'PUT':
+        if not request.is_json:
+            abort(400)
+        data = request.get_json()
+        for key in data.keys():
+            if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+                setattr(city_inst, key, data[key])
+        city_inst.save()
+        return make_response(jsonify(city_inst.to_dict()), 200)
