c18786f74cb46a64569d85ac11e6d1cc35f42b1c
AlexDelei <alexdelei125@gmail.com>
2024-03-31 14:56:18 +0000

    right jsonfiy

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,9 @@
 should be the json data
 """
 from api.v1.views import app_views
-from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'])
 def isItOk():
     """return its JSON data"""
-    return jsonify({
-                  'stauts': 'OK'
-                  })
+    return "{\n  \"status\": \"OK\"\n}\n"
