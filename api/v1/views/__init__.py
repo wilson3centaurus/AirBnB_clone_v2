4ef0d67644333408155972d48b5ff96c136d50ad
waltertaya <126944679+waltertaya@users.noreply.github.com>
2024-04-01 22:39:55 +0300

    Added: Task 7

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,8 @@
 """ Blueprint for the views """
 
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__)
-
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+
+app_views = Blueprint('app_views', __name__)
