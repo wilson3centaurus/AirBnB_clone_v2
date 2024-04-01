ad64afae5769535070668949679aa11b30905a74
ukaemma2 <ukaemmanuel2000@gmail.com>
2024-04-01 14:41:21 +0000

    views

--- models/__init__.py
+++ models/__init__.py
@@ -3,15 +3,16 @@
 initialize the models package
 """
 
-from os import getenv
+import os
+"""CNC - dictionary = { Class Name (string) : Class Type }"""
 
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine import db_storage
+    CNC = db_storage.DBStorage.CNC
+    storage = db_storage.DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+    from models.engine import file_storage
+    CNC = file_storage.FileStorage.CNC
+    storage = file_storage.FileStorage()
+
 storage.reload()
