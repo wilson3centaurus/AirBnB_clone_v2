5f447f015cc4dc736b9c9246fe0be657a8044cca
AxiomDays <tobeoke@gmail.com>
2024-04-01 18:29:34 +0100

    fxn name change

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,24 +3,24 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.state import Amenity
+from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-def get_all():
+def amenities_get_all():
     states_dict = []
     for state in storage.all(Amenity).values():
         states_dict.append(state.to_dict())
     return jsonify(states_dict)
 
 @app_views.route('/amenities/<path:amenity_id>', methods=['GET'], strict_slashes=False)
-def get_state(amenity_id):
+def amenities_get_state(amenity_id):
     state = storage.get(Amenity, amenity_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
 @app_views.route('/amenities/<path:amenity_id>', methods=['DELETE'], strict_slashes=False)
-def delete_state(amenity_id):
+def amenities_delete_state(amenity_id):
     if amenity_id is None:
         abort(404)
     state = storage.get(Amenity, amenity_id)
@@ -31,7 +31,7 @@ def delete_state(amenity_id):
     return jsonify({})
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-def create_state():
+def amenities_create_state():
     state = request.get_json()
     if type(state) != dict:
         return abort(400, {'message': 'Not a JSON'})
@@ -42,7 +42,7 @@ def create_state():
     return jsonify(buff.to_dict()), 201
 
 @app_views.route('/amenities/<path:amenity_id>', methods=['PUT'], strict_slashes=False)
-def update_state(amenity_id):
+def amenities_update_state(amenity_id):
     if amenity_id is None:
         abort(404)
     state = storage.get(Amenity, amenity_id)
