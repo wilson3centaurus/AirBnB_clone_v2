e181bb23a73d6bd01325b87f9695b927805bd934
RadouaneAbn <radouaneabn@gmail.com>
2024-04-01 20:53:32 +0000

    jsonify all_cities

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,7 +4,7 @@ view for Place objects that handles
 all default RESTFul API actions
 """
 from api.v1.views import app_views
-from flask import Blueprint, abort, request
+from flask import abort, request, jsonify
 from werkzeug.exceptions import BadRequest
 from models import storage
 from models.city import City
@@ -12,24 +12,16 @@ from models.place import Place
 from models.user import User
 
 
-def get_object_by_id(cls, obj_id):
-    """  """
-    for _, obj in storage.all(cls).items():
-        if obj.id == obj_id:
-            return obj
-    return None
-
-
 @app_views.route(
         "/cities/<city_id>/places",
         methods=['GET'],
         strict_slashes=False)
 def retrive_places_by_city(city_id):
-    """  """
-    city = get_object_by_id(City, city_id)
+    """ This function return list of places users """
+    city = storage.get(City, city_id)
     if not city:
         abort(404)
-    return [place.to_dict() for place in city.places]
+    return jsonify([place.to_dict() for place in city.places])
 
 
 @app_views.route(
@@ -37,8 +29,10 @@ def retrive_places_by_city(city_id):
         methods=['GET'],
         strict_slashes=False)
 def retrive_place(place_id):
-    """  """
-    place = get_object_by_id(Place, place_id)
+    """ This function is used to retrive a specific place
+        object using its id
+    """
+    place = storage.get(Place, place_id)
     if not place:
         abort(404)
     return place.to_dict()
@@ -49,7 +43,10 @@ def retrive_place(place_id):
         methods=['DELETE'],
         strict_slashes=False)
 def delete_place(place_id):
-    place = get_object_by_id(Place, place_id)
+    """ This function is used to delete a place object when
+        the DELETE method is called
+    """
+    place = storage.get(Place, place_id)
     if not place:
         abort(404)
     storage.delete(place)
@@ -62,7 +59,9 @@ def delete_place(place_id):
         methods=['POST'],
         strict_slashes=False)
 def create_place(city_id):
-    city = get_object_by_id(City, city_id)
+    """ This function creates a new place object
+    """
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
     try:
@@ -73,7 +72,7 @@ def create_place(city_id):
     if 'user_id' not in request_data:
         abort(400, "Missing user_id")
 
-    user = get_object_by_id(User, request_data['user_id'])
+    user = storage.get(User, request_data['user_id'])
     if user is None:
         abort(404)
 
@@ -91,7 +90,9 @@ def create_place(city_id):
         methods=['PUT'],
         strict_slashes=False)
 def update_place(place_id):
-    place = get_object_by_id(Place, place_id)
+    """ This function updates an existing place object
+    """
+    place = storage.get(Place, place_id)
     if not place:
         abort(404)
     try:
