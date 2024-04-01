2859ed0220904d723d15274d58d03858d926197d
Anthony <ndegwaanthony300@gmail.com>
2024-04-02 00:31:24 +0300

    404 file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python:wq
-3
+#!/usr/bin/python
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -7,13 +7,21 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle the 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Closes the storage connection"""
     storage.close()
 
+
 if __name__ == "__main__":
     import os
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, timeout=60)
