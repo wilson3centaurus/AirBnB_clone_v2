b85ce1c13c8750fdea3387dd765c363e9785a3f6
AlexDelei <alexdelei125@gmail.com>
2024-04-01 09:26:24 +0000

    count of objects in storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 """
-should be the json data
+all objects in storage
 """
+import models
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status', methods=['GET'])
 def isItOk():
     """return its JSON data"""
     return "{\n  \"status\": \"OK\"\n}\n"
+
+
+@app_views.route('/stats', methods=['GET'])
+def all_objects():
+    """return all objects in storage engine"""
+    all_dict = {
+             'amenities': storage.count(Amenity),
+             'cities': storage.count(City),
+             'places': storage.count(Place),
+             'reviews': storage.count(Review),
+             'states': storage.count(State),
+             'users': storage.count(User)
+           }
+    return (jsonify(all_dict))
