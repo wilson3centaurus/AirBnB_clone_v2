ef474a9640be3378478d28df4edae00aa568623a
rabiebn <rabie.bnmr@gmail.com>
2024-04-01 22:11:13 +0000

    14: hash users passwords

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+from typing import Any
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +29,12 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+
+    def __setattr__(self, __key, __value):
+        """sets attributes for User obj"""
+        if __key == "password":
+            pwd_hash = hashlib.md5(__value.encode('utf-8'))
+            super().__setattr__(__key, pwd_hash.hexdigest())
+        else:
+            super().__setattr__(__key, __value)
