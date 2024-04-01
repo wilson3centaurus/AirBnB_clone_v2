280e196a7b1084b45406f7b6b77adf8d81caf47f
Max Asante <ink@Maxs-MacBook-Air.local>
2024-04-01 10:44:38 +0300

    placess amenities module

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Views for the place_amenity module"""
+import os
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+
+
+@app_views.route('/places/<string:place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_place_amenities(place_id):
+    """ retrieves all amenities from a place """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenities = [obj.to_dict() for obj in place.amenities]
+    return jsonify(amenities)
+
+
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_places_amenity(place_id, amenity_id):
+    """ delete amenity from place """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def post_place_amenity(place_id, amenity_id):
+    """ post amenity by id for a place """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity in place.amenities:
+        return (jsonify(amenity.to_dict()), 200)
+    place.amenities.append(obj)
+    storage.save()
+    return (jsonify(amenity.to_dict(), 201))
