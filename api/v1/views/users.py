eda783dd8cf7b5b988d10c1d3512cbcb4a0d1f73
RadouaneAbn <radouaneabn@gmail.com>
2024-04-01 05:34:13 +0000

    try

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -62,9 +62,10 @@ def delete_user(user_id):
 def create_user():
     """ This function creates a new user object
     """
-    request_data = request.get_json(silent=True)
-    if not request_data:
-        abort(400, "Not a JSON")
+    try:
+        request_data = request.get_json()
+    except Exception:
+        abort(400, description="Not a JSON")
     if "email" not in request_data:
         abort(400, "Missing email")
     if "password" not in request_data:
@@ -85,9 +86,10 @@ def update_user(user_id):
     user = get_object_by_id(User, user_id)
     if not user:
         abort(404)
-    request_data = request.get_json(silent=True)
-    if not request_data:
-        abort(400, "Not a JSON")
+    try:
+        request_data = request.get_json()
+    except Exception:
+        abort(400, description="Not a JSON")
 
     for key, value in request_data.items():
         if key not in ('id, email, created_at, updated_at'):
