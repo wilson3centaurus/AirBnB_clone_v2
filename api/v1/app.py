8eb0ca2b605042f39da4c6db16aecdb997fd3d7e
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-04-01 08:16:34 -0700

    fix CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, ressources={r"/api/v1/*": {"origins": ["0.0.0.0"]}},
-            origins="0.0.0.0")
+cors = CORS(app, resources={r"/api/v1/*": {"origins": ["0.0.0.0"]}})
 
 
 @app.teardown_appcontext
