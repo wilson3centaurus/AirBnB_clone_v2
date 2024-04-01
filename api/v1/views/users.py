3129a33a982282d61602adeca38b86025eeaec23
OjElsa <elsahojwang06@gmail.com>
2024-04-01 15:57:53 -0700

    User Object Of a Place

---
+++ api/v1/views/users.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""View for users that handles all RESTful API actions"""
+
+from flask import jsonify, request, abort
+from models import storage
+from models.user import User
+from api.v1.views import app_views
+
+
+@app_views.route("/users", methods=["GET"], strict_slashes=False)
+def get_all_user():
+    """Returns a list of all users"""
+    users = storage.all(User).values()
+    return jsonify([user.to_dict() for user in users])
+
+
+@app_views.route("/users/<user_id>", methods=["GET"],
+                 strict_slashes=False)
+def get_user(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route("/users", methods=["POST"], strict_slashes=False)
+def post_user():
+    """Adds a new user"""
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if "email" not in data:
+        abort(400, "Missing email")
+    if "password" not in data:
+        abort(400, "Missing password")
+    user = User(**data)
+    user.save()
+    return jsonify(user.to_dict()),201
+
+
+@app_views.route("/users/<user_id>", methods=["PUT"], strict_slashes=False)
+def update_user(user_id):
+    """Update a user"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    ignore_keys = ["id", "created_at", "updated_at"]
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
