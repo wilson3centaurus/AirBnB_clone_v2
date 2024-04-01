21d59eb66b79efcf50104e92840b95a089b0b6d6
Anthony <ndegwaanthony300@gmail.com>
2024-04-02 00:40:44 +0300

    states objects handling restapi

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python3
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
+
 
 @app_views.route('/states', methods=['GET'])
 def get_states():
     """Retrieve list of all State objects"""
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
+
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
@@ -16,6 +19,7 @@ def get_state(state_id):
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
+
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
@@ -26,6 +30,7 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states', methods=['POST'])
 def create_state():
@@ -38,6 +43,7 @@ def create_state():
     new_state = State(**data)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
