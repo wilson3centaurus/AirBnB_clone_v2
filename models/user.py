cc3f853e1d6d4b1461792d0845fdfc2b97496a23
RadouaneAbn <radouaneabn@gmail.com>
2024-04-01 21:45:44 +0000

    revert

--- models/user.py
+++ models/user.py
@@ -2,9 +2,11 @@
 """ holds class User"""
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
+import hashlib
 from sqlalchemy import Column, String
+import hashlib
+from models.base_model import BaseModel, Base
+import models
 from sqlalchemy.orm import relationship
 
 
@@ -16,10 +18,14 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                                cascade="all, delete, delete-orphan")
+        places = relationship(
+                "Place",
+                backref="user",
+                cascade='all, delete, delete-orphan')
+        reviews = relationship(
+                "Review",
+                backref="user",
+                cascade='all, delete, delete-orphan')
     else:
         email = ""
         password = ""
