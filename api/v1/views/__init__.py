dba41dee95c29be5a771daa7c567469d34a8baf0
MarcieD1 <marciatarisai@icloud.com>
2024-04-01 21:59:52 +0200

    Fix PEP8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,5 @@ from api.v1.views.states import *
 from api.v1.views.cities import  # Import the new file here
 from api.v1.views.amenities import
 from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
