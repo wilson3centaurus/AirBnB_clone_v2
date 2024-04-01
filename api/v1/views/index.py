f2da1ef61f353ad7183f7059f34dd8f7cc299f61
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-01 22:07:43 +0300

    task 3wq

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,23 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Returns the status of the API."""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Get the number of each object type"""
+    stats = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+    }
+    return jsonify(stats)
