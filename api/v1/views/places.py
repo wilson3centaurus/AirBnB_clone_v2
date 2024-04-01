4c2ee57bf738a7e8d6f658f1684d61f4e36087fa
RadouaneAbn <radouaneabn@gmail.com>
2024-04-01 21:31:11 +0000

    add city_id to requested_data

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -74,8 +74,8 @@ def create_place(city_id):
     if 'name' not in request_data:
         abort(400, "Missing name")
 
+    request_data['city_id'] = city_id
     new_place = Place(**request_data)
-    new_place.city_id = city_id
     new_place.save()
     return jsonify(new_place.to_dict()), 201
 
