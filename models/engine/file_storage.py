d6db86b2f067d114a35c3bdd176f28d7a331696d
iDamina <daminailiya@gmail.com>
2024-04-01 04:04:52 +0100

    update to db_storage, file_storage and their test cases

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception as ex:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,21 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        if (cls in classes.values() and
+                id and
+                isinstance(id, str)):
+            ret_obj = self.all(cls)
+            for key, value in ret_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """ method to count number of objects """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+            return len(data)
