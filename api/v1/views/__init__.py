3673290eb3699218cd3640b1f79db57145c3a166
Anthony <ndegwaanthony300@gmail.com>
2024-04-01 23:49:40 +0300

    initial commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 
 from . import index
@@ -6,13 +5,6 @@ from flask import Blueprint
 
 from api.v1.views.index import app_views
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-=======
-from flask import Blueprint
 
 # Create a Blueprint instance with URL prefix /api/v1
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Wildcard import of everything in the package api.v1.views.index
-from . import index
->>>>>>> f75b135 (initial commit)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,26 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 
 from . import app_views
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
 
-=======
-from . import app_views
-from flask import jsonify
-
->>>>>>> f75b135 (initial commit)
 # Define a route /status on the object app_views
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def stats():
+    """Retrieve stats of objects by type"""
+    counts = {}
+    classes = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+
+    for cls_name in classes:
+        cls = getattr(storage, cls_name)
+        count = storage.count(cls)
+        counts[cls_name] = count
+
+    return jsonify(counts)
