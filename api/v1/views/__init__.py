b6794defde83ec7b2cb09713a1313d88a3fd3643
Anthony <ndegwaanthony300@gmail.com>
2024-04-02 00:09:05 +0300

    airbnb code updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-from . import index
 from flask import Blueprint
+from api.v1.views.index import *
 
-from api.v1.views.index import app_views
 
-
-# Create a Blueprint instance with URL prefix /api/v1
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-from . import app_views
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
