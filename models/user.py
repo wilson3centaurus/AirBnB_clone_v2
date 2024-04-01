df8c17fabb02f1518dd8b23483d6ee84c49607ba
RadouaneAbn <radouaneabn@gmail.com>
2024-04-01 05:44:22 +0000

    task 12 undone

--- models/user.py
+++ models/user.py
@@ -47,4 +47,6 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        """
         self.password = hashlib.md5(kwargs['password'].encode()).hexdigest()
+        """
