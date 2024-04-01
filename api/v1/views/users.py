ea5957964bee5d7590adb02b7f4518b8fe9ee729
bini34 <biniyamambachew@gmail.com>
2024-04-01 22:55:22 +0300

    update

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""users module"""
+"""Module for user-related API endpoints."""
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
