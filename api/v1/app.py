75534de08e4aa7cec74ad1fc8c47eb4dbbea7653
MarcieD1 <marciatarisai@icloud.com>
2024-04-02 14:53:20 +0200

    setting env

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,47 @@
 #!/usr/bin/python3
-"""app.py to connect to API Flask"""
+"""
+app
+"""
+import sys
+print(sys.path)
 
 from flask import Flask, jsonify
-from models import storage
-from api.v1.views import app_views
-from os import getenv
 from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
 
 
-@app.errorhandler(404)
-def page_not_found(exception):
-    return (jsonify({"error": "Not found"}), 404)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage upon teardown"""
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
-# Add a blank line here
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST') and getenv('HBNB_API_PORT'):
-        app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'),
-                threaded=True)
-    else:
-        app.run(host='0.0.0.0', port='5000', threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
