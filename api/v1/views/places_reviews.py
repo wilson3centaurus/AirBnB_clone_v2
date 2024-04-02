0fd42cb3e90d92f3652c8f153038f5fff7601530
DTAJ095 <albanjaures26@gmail.com>
2024-04-02 02:30:20 +0100

    update

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -16,7 +16,9 @@ def get_all_reviews(place_id):
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    reviews = [review.to_dict() for review in place.reviews]
+    reviews = []
+    for review in place.reviews:
+        reviews.append(review.to_dict())
     return jsonify(reviews)
 
 
