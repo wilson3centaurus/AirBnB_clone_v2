4b8caef776414a178721b50273a3581718070f4f
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-04-01 17:58:56 -0700

    pushing on the master now

---
+++ v1/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+implement status route
+return the status of API
+"""
+
+
+from flask import Flask, jsonify, make_response
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+    close storage session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("HBNB_API_HOST", "0.0.0.0"),
+            port=int(os.environ.get("HBNB_API_PORT", "5000")), threaded=True)
