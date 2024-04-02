4f423641e7a1c240be21e839c3d74a8666823070
michael <michaelhagos163@gmail.com>
2024-04-02 15:37:14 +0000

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""This module define a blueprint for routes with Blueprint object"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
