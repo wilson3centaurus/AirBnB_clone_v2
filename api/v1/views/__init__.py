dcd60945effa9f3ac7997878c0ceb32bd457c4d8
MarcieD1 <marciatarisai@icloud.com>
2024-04-02 20:00:11 +0200

    Fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,7 @@ views
 
 from flask import Blueprint
 
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -15,4 +15,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
-from api.v1.views import app_views
