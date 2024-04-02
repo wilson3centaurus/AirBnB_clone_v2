15f239d5371efcae6e9efbaf53a4d50d14ee0d3b
michael <michaelhagos163@gmail.com>
2024-04-02 15:30:06 +0000

    update on app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,20 +12,20 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown_appcontext(self):
     """
     Teardown App Context.
     """
-    storage.close()
+    return storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     '''return render_template'''
-    return jsonify('error='Not found'), 404
+    return jsonify("error=" "Not found"), 404
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
