36d0009d3f23fd9b9f0a0331b520e1af868c5083
AlexDelei <alexdelei125@gmail.com>
2024-04-03 01:30:26 +0000

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,11 @@ from models import storage
 from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
