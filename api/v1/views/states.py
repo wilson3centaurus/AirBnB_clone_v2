fcb91639513e1014a9b05e694c6e5bcfc42c8025
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-04-03 18:40:47 -0700

    still getting red checks

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,89 +1,73 @@
 #!/usr/bin/python3
-"""DOCSTRING FOR MODULE"""
-from api.v1.views import app_views
+"""Import required modules"""
+from flask import Flask, make_response
 from flask import abort, jsonify, request
-from models import storage
 from models.state import State
+from api.v1.views import app_views
+from os import getenv
+import json
 
 
-@app_views.route('/states/', methods=['GET'], strict_slashes=False)
-def show_states():
-    """retrieves list of all State objects"""
-    states = storage.all('State').values()
-    states_list = []
-    for state in states:
-        states_list.append(state.to_dict())
-    return jsonify(states_list)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_list_of_all_states():
+    """Retrieves the list of all State objects"""
+    from models import storage
+    storage.reload
+    states_obj = storage.all(State).values()
+    states_dict = [state.to_dict() for state in states_obj]
+    return jsonify(states_dict)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def show_state(state_id):
-    """retrieves a State object by state_id"""
-    state = storage.get('State', state_id)
-    if state is not None:
+def get_state_obj(state_id):
+    """Retrieves a State object"""
+    from models import storage
+    storage.reload
+    state = storage.get(State, state_id)
+    if state:
         return jsonify(state.to_dict())
-    else:
-        abort(404)
+    abort(404)
 
 
 @app_views.route(
         '/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def delete_state(state_id):
-    """
-        deletes a State object by state_id, returns empty dictionary on success
-    """
-    state = storage.get('State', state_id)
-    if state is not None:
+def delete_state_ob(state_id):
+    """Deletes a State object"""
+    from models import storage
+    storage.reload
+    state = storage.get(State, state_id)
+    if state:
         storage.delete(state)
         storage.save()
-        return jsonify({})
-    else:
-        abort(404)
+        return jsonify({}), 200
+    abort(404)
 
 
-@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    """creates a State object"""
-    content = request.get_json(silent=True)
-    error_message = ""
-    if type(content) is dict:
-        if "name" in content.keys():
-            state = State(**content)
-            storage.new(state)
-            storage.save()
-            response = jsonify(state.to_dict())
-            response.status_code = 201
-            return response
-        else:
-            error_message = "Missing name"
-    else:
-        error_message = "Not a JSON"
-    response = jsonify({'error': error_message})
-    response.status_code = 400
-    return response
+    """Creates a State"""
+    if not request.get_json():
+        return jsonify({"error": "Not a JSON"}), 400
+    if "name" not in request.get_json():
+        return jsonify({"error": "Missing name"}), 400
+    data = request.get_json()
+    new_state = State(**data)
+    from models import storage
+    new_state.save()
+    return (new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """updates a State object"""
-    state = storage.get('State', state_id)
-    error_message = ""
+    """Updates a State object"""
+    data = request.get_json()
+    if not request.get_json():
+        return jsonify({"error": "Not a JSON"}), 400
+
+    from models import storage
+    state = storage.get(State, state_id)
     if state:
-        content = request.get_json(silent=True)
-        if type(content) is dict:
-            ignore = ['id', 'created_at', 'updated_at']
-            for key, value in content.items():
-                if key in ignore:
-                    continue
-                else:
-                    setattr(state, key, value)
-            storage.save()
-            response = jsonify(state.to_dict())
-            response.status_code = 200
-            return response
-        else:
-            error_message = "Not a JSON"
-            response = jsonify({'error': error_message})
-            response.status_code = 400
-            return response
-    abort(404)
+        state.name = data['name']
+        state.save()
+        return (state.to_dict())
+    abort(404)  # If no matching state is found, return a 404 error
