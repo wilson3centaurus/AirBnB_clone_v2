23229496ac0dfa4cada8370d45a977eb0473c6b6
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-04-03 18:44:13 -0700

    still red checks

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,11 +9,20 @@ return the status of API
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors that returns a
+    JSON-formatted 404 status code response"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.teardown_appcontext
