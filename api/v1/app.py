299ff786334f8474ae1bebeb8cfcc935629c3131
VandevilleF <flo.vandeville@gmail.com>
2024-04-16 22:18:53 +0200

    Add file for task 6, local test OK

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Start your API"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def close(exception):
     """Close storage"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(exception):
+    """A handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
