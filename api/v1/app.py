4b51c8ca9aa4620540501d9b77e4fe8c452f6501
VandevilleF <flo.vandeville@gmail.com>
2024-04-16 15:46:23 +0200

    Change import in __init__ for import everything in the package and parametre of host and port in app file for define the environement variable

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 
@@ -18,5 +19,9 @@ def close(exception):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
-# app.run(host="HBNB_API_HOST", port="HBNB_API_PORT", threaded=True)
+    # Set host with HBNB_API_HOST env var or '0.0.0.0' if not defined
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    # Set port with HBNB_API_PORT env var or 5000 if not defined
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    # Run the Flask server with the specified parameters
+    app.run(host=host, port=port, threaded=True)
