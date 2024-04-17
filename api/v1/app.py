f37dce5c23a0cb87f326ac844bf1d5a150ad39cb
savvyh <7277@holbertonstudents.com>
2024-04-17 13:50:45 +0000

    Adding a method that handle error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """variables and methods used to connect to API"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,13 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Closes the storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    '''Returns a JSON error 404
+    '''
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
