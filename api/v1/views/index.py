7a3b958601b7c1885ab4c99e213f5acb4687f45d
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-16 22:14:45 -0400

    Added route for /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,35 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'], srtict_slahes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     '''Returns JSON with status'''
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = {
+        Amenity: 'amenities',
+        City: 'cities',
+        Place: 'places',
+        Review: 'reviews',
+        State: 'states',
+        User: 'users'
+        }
+
+    objects = {}
+    for cls, value in classes.items():
+        objects[value] = storage.count(cls)
+
+    return jsonify(objects)
