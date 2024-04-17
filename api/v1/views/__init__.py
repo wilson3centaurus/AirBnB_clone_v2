db4b5d1de858ad858d4452863851b4220aa86330
savvyh <7277@holbertonstudents.com>
2024-04-17 13:32:37 +0000

    Add some documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""initializes view of directory"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
