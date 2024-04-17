881ccca9dd25ffd8f422f407e50e963c82c22db6
Arweenn <arwendumont25@outlook.fr>
2024-04-17 13:45:49 +0200

    Task 4 - Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for the API.
+"""
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/status')
+def status():
+    """Status of the API"""
+
+    return jsonify({"status": "OK"})
+    
