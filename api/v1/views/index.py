e5ffe08c6ce86d2fca4fb038a046de5e19f8de96
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-16 21:29:42 -0400

    Added strict_slahes false to route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], srtict_slahes=False)
 def status():
     '''Returns JSON with status'''
     return jsonify({"status": "OK"})
