9ae3b544a9e282b92c10fe1dbf0c0400d5dcf83a
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-18 13:09:20 +0200

    add api directory

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Index for API routes v1"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Returns a json for the state of the API"""
+    return jsonify({'status': 'OK'})
