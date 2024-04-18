2f915b81a06a744579531e364fe01e80c56d2958
savvyh <7277@holbertonstudents.com>
2024-04-18 10:13:07 +0000

    Come back to the previous code

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """initializes view of directory"""
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
