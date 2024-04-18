31fa05108c95482767cbe6cf6e0c6b8be555e390
lucieruin <lucie.wattez@hotmail.fr>
2024-04-18 14:44:59 +0200

    correct pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,6 +17,7 @@ def status():
     """ Status of API """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """ stats of API"""
