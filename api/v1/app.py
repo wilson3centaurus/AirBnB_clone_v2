b9225e7c0d5bc3b91f00e1d9fa11498255bcb1ba
lchenevi <leo.cheneviere@gmail.com>
2024-04-18 15:04:44 +0200

    pushing task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
-
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+# Import storage from models
+# Import app_views from api.v1.views
+# Register the blueprint app_views to your Flask instance app
 app.register_blueprint(app_views)
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Cré une route pour gérer les erreurs 404"""
-    return jsonify(error="Not found"), 404
-
-
+# Declare a method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
-def close_storage(exception):
-    """Ferme la connexion à la base de données après chaque requête."""
+def teardown_storage(exception):
     storage.close()
 
-
 if __name__ == "__main__":
-    """Récupére les valeurs des variables d'environnement
-    ou utilise des valeurs par défaut"""
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
-
-    """Lancez l'application Flask"""
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,9 @@
 #!/usr/bin/python3
 """defines endpoints and their output """
-from flask import Blueprint, jsonify
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from api.v1.views import app_views
+from flask import jsonify
 
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
-@app_views.route('/status', strict_slashes=False)
+# Create a route /status on the object app_views that returns a JSON: "status": "OK"
+@app_views.route('/status', methods=['GET'])
 def get_status():
-    """Créez une route /status qui renvoie un JSON
-    avec la clé "status" et la valeur "OK"""
     return jsonify({"status": "OK"})
-
-
-@app_views.route("/stats", strict_slashes=False)
-def count_objs():
-    """count obj"""
-    stats_dict = {
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review),
-        'states': storage.count(State),
-        'users': storage.count(User)
-    }
-    return jsonify(stats_dict)
