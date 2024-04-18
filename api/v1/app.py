91ae5015cc301d06043ee71726b479ad0ea96998
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-18 13:17:31 +0200

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(self):
     """closes the storage on teardown"""
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Define a Blueprint for API routes v1"""
 from flask import Blueprint
+from api.v1.views.index import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
