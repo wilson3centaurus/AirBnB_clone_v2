fc8fdda9cda87e637ebe09d27a999a92bb51a992
Kayme976 <7508@holbertonstudents.com>
2024-04-18 16:10:48 +0200

    Refactor API routes for amenities and cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,17 +16,19 @@ def states_id_cities(id):
         Flask route at /states/<id>/cities.
     """
     state = storage.get(State, id)
-    if (state):
-        if request.method == 'POST':
-            kwargs = request.get_json()
-            if not kwargs:
-                return {"error": "Not a JSON"}, 400
-            if "name" not in kwargs:
-                return {"error": "Missing name"}, 400
-            new_city = City(state_id=id, **kwargs)
-            new_city.save()
-            return new_city.to_dict(), 201
+    if not state:
+        abort(404)
 
-        elif request.method == 'GET':
-            return jsonify([c.to_dict() for c in state.cities])
-    abort(404)
+    if request.method == 'POST':
+        kwargs = request.get_json()
+        if not kwargs:
+            return jsonify({"error": "Not a JSON"}), 400
+        if "name" not in kwargs:
+            return jsonify({"error": "Missing name"}), 400
+        new_city = City(state_id=id, **kwargs)
+        new_city.save()
+        return jsonify(new_city.to_dict()), 201
+
+    elif request.method == 'GET':
+        return jsonify([c.to_dict() for c in state.cities])
+    
