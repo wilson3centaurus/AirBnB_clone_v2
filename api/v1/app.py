e85197397f6bf93dc56ecb72d85797395ea53547
MLK59 <abdelmalekmhamed@gmail.com>
2024-04-18 14:37:27 +0200

    Add a 404 error handler to return a JSON status code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """start flask web api"""
 
+from flask import jsonify
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown_db(self):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a JSON-formated status code for errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
