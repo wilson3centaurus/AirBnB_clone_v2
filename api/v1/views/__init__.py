07a318b8c284068fb7f241401a1b0f5fa8859d80
Livanhernandez <livanhernandez9@gmail.com>
2024-04-18 16:52:44 -0400

    created amenities and updated __init__ file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,9 +4,8 @@
 '''
 from flask import Blueprint
 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Create the blueprint object
-app_views = Blueprint('app_views', __name__,
-                      url_prefix='/api/v1')
-
+# Import all view modules here
 from api.v1.views.index import *
+from api.v1.views.amenities import *
