5b4b050742b95749c46963a6e5df64a8b6aec8b2
prodjohnper <JonathanPerez9743@gmail.com>
2024-04-17 23:14:09 -0400

    Fix - Fixed error with circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,11 @@
 '''
     Initialize the blueprints for the API
 '''
-from api.v1.views.index import *
 from flask import Blueprint
 
 
 # Create the blueprint object
 app_views = Blueprint('app_views', __name__,
                       url_prefix='/api/v1')
+
+from api.v1.views.index import *
