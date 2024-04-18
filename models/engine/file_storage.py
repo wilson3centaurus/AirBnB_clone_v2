5f0297f72abf53713c60933da9b4d2d234807fed
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-18 00:50:52 -0400

    ADD - self.save() after deleting object inside the 'def delete()'

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,6 +68,7 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+                self.save()
 
     def get(self, cls, id):
         '''
