5e9c04fadb1fcf4ff05a9033268118e13aac5a7e
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-18 16:12:32 +1000

    feat(task 13): add CROS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,13 @@
 This module provides flask app routing certain view pages.
 """
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
