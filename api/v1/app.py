caf3c5afc5ac0e67d58fdb82eff3487ddccaf74d
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-17 20:58:41 -0400

    Comenting another way to test

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,16 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, make_response, jsonify
-
+from flask_restplus import Api
 
 app = Flask(__name__)
+api = Api(
+    app,
+    version='0.1',
+    title='Your API Title',
+    description='Your API Description'
+    )
+
 app.register_blueprint(app_views)
 
 
@@ -18,13 +25,14 @@ def close_db(error):
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    """ 404 Error """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@api.errorhandler
+def default_error_handler(e):
+    """ Default Error Handler """
+    return {'message': 'An unhandled exception occurred.'}, 500
 
 
 if __name__ == "__main__":
