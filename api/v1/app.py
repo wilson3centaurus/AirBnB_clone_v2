d53553a06b300dce8f536cbc837218e14e72a10e
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-18 14:25:56 -0400

    Testinfg documentation Jesus helping

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" Flask Application 0.1 """
-
+""" Flask Application """
 from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -28,7 +32,11 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    # Main function
-    host = environ.get('HBNB_API_HOST') or '0.0.0.0'
-    port = environ.get('HBNB_API_PORT') or '5000'
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
