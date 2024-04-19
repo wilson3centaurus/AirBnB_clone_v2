d1ab5b6ef3cf9aa7ffaf68c488740535010c592b
Daniel Colorado <danielcoloradol@hotmail.com>
2024-04-19 21:54:59 +1000

    feat(task 4):Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import os
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+
+# link the blueprint to the app (FLask obj)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(exc):
+    """Close the db session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host_d = os.getenv("HBNB_API_HOST") or "0.0.0.0"
+    port_d = os.getenv("HBNB_API_PORT") or 5000
+    app.run(host=host_d, port=5000, threaded=True)
