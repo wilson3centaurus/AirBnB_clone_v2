b2d8f2e0133f427cd6b984f24b722347fc825424
Kayme976 <7508@holbertonstudents.com>
2024-04-19 02:02:33 +0200

    Refactor teardown method in app.py and add error handler for 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,14 +14,18 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Teardown method to close the storage"""
+def teardown_db(exception):
+    """Closes the storage on teardown"""
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(exception):
+    """Returns the 404 error custom messsage"""
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
-
-    app_host = getenv("HBNB_API_HOST", "0.0.0.0")
-    app_port = int(getenv("HBNB_API_PORT", 5000))
-
-    app.run(host=app_host, port=app_port, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True, debug=True)
