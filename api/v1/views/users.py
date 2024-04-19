0ac5a95da2af35e569b0beee40cea74cdfd1c698
MLK59 <abdelmalekmhamed@gmail.com>
2024-04-19 15:30:08 +0200

    add user file that create, update and delete

---
+++ api/v1/views/users.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+"""user_management.py"""
+
+from flask import abort, jsonify, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+# Route definition for retrieving all users
+# ============================================================================
+
+@app_views.route('/api/v1/users', methods=['GET'], strict_slashes=False)
+def get_users():
+
+    """Retrieves a list of all User objects in the system"""
+
+    users = storage.all(User).values()
+    list_users = [user.to_dict() for user in users]
+    return jsonify(list_users)
+
+
+# Route definition for retrieving a specific user
+# ============================================================================
+@app_views.route('/users/<string:user_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_user(user_id):
+    """
+    Retrieves a specific User object based on its ID.
+
+    Args:
+        user_id (str): The ID of the user to retrieve.
+
+    Returns:
+        A JSON response containing the user dictionary or a 404 error if not found.
+    """
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+# Route definition for deleting a user
+# ============================================================================
+@app_views.route('/users/<string:user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_user(user_id):
+    """
+    Deletes a specific User object based on its ID.
+
+    Args:
+        user_id (str): The ID of the user to delete.
+
+    Returns:
+        An empty JSON response with a status code of 200 on success or a 404 error if not found.
+    """
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return jsonify({})
+
+
+# Route definition for creating a new user
+# ============================================================================
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+   """
+    Creates a new User object in the system"""
+   user_data = request.get_json()
+   if not user_data:
+        return make_response(jsonify({'error': 'Missing JSON data'}), 400)
+   if 'email' not in user_data:
+        return make_response(jsonify({'error': 'Email is required'}), 400)
+   if 'password' not in user_data:
+        return make_response(jsonify({'error': 'Password is required'}), 400)
+   new_user = User(**user_data)
+   new_user.save()
+   return make_response(jsonify(new_user.to_dict()), 201)
+
+
+# Route definition for updating a user
+# ============================================================================
+@app_views.route('/users/<string:user_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_user(user_id):
+    """
+    Updates an existing User object in the system.
+
+    Args:
+        user_id (str): The ID of the user to update.
+
+    Returns:
+        A JSON response containing the updated user dictionary with a status code of 200,
+        or a 400 error with a message if the request is invalid or a 404 error if not found.
+    """
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+    user_data = request.get_json()
+    if not user_data:
+        return make_response(jsonify({'error': 'Missing JSON data'}), 400)
+    for field, value in user_data.items():
+        if field not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, field, value)
+    user.save()
+    return jsonify(user.to_dict())
