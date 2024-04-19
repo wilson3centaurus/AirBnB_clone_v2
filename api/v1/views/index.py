fb258f3b3e3e4e08b96a0a6be6e72154fa2c8959
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-19 17:28:58 -0400

    endpoint that retrieves the number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,18 +16,22 @@ if __name__ == "__main__":
     pass  # Add any additional logic if required
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', method=['GET'], strict_slashes=False)
 def status():
-    """returns a JSON"""
+    """endpoint that returns the status of the API"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    """Retrieves the number of each objects by type"""
-    classes = {"amenities": Amenity, "cities": City, "places": Place,
-               "reviews": Review, "states": State, "users": User}
-    count_dict = {}
-    for key, value in classes.items():
-        count_dict[key] = storage.count(value)
-    return jsonify(count_dict)
+    """Endpoint to retrieve stats of objects by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    print("Status:", stats)
+    return jsonify(stats)
