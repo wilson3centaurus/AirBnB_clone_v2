d42bfe50a80e2f864f794cbbcac2a40c5491ee33
Daniel Colorado <danielcoloradol@hotmail.com>
2024-04-19 22:20:36 +1000

    feat(task 5):Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,8 @@
 """ Module containing all routes belonging to app_views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.engine.db_storage import classes
 
 
 @app_views.route("/status")
@@ -11,3 +13,25 @@ def index():
     """
     message = {"status": "OK"}
     return jsonify(message)
+
+
+@app_views.route("/stats")
+def stats():
+    """
+    retrieves the number of each objects by type
+    """
+    output = {}
+    for key, val in classes.items():
+        if key == "Amenity":
+            output["amenities"] = storage.count(val)
+        if key == "City":
+            output["cities"] = storage.count(val)
+        if key == "Place":
+            output["places"] = storage.count(val)
+        if key == "Review":
+            output["reviews"] = storage.count(val)
+        if key == "State":
+            output["states"] = storage.count(val)
+        if key == "User":
+            output["users"] = storage.count(val)
+    return jsonify(output)
