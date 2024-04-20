4342d88611aaa4553d220bd568755e788c7a6496
Zona <echyth@yahoo.com>
2024-04-20 13:38:23 +1000

    fix(task 6): update not_found route response status (#4)
    * fix(task 1): correct style errors in console.py
    
    * fix(task 6): update not_found route response status

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,7 +20,7 @@ def close_db(exc):
 @app.errorhandler(404)
 def not_found(e):
     """handle 404 error"""
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
