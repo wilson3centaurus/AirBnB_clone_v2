79eb3e84ed007217876afdb4de67284695394737
Livanhernandez <livanhernandez9@gmail.com>
2024-04-19 21:30:41 -0400

    fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Index module for Flask app."""
 
-
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 from models.city import City
@@ -23,12 +22,11 @@ def api_status():
 def get_stats():
     """Endpoint to retrieve stats of objects by type."""
     stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
-    print("Stats:", stats)
     return jsonify(stats)
