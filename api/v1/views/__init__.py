b85628039c2e06d4c50c47c557f18383fe6de7e4
Warren <rabbitsinwarrens@gmail.com>
2024-04-20 19:11:23 +1000

    fix(task1): update code to satisfy style requirements of checker

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,9 @@
 from flask import Blueprint
-
+from api.v1.views.index import *
+from api.v1.views.states import *
 
 # defining a blueprint
 app_views = Blueprint(
     "app_views",
     __name__,
     url_prefix="/api/v1")
-
-from api.v1.views.index import *
-from api.v1.views.states import *
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,11 +9,12 @@ from api.v1.views import app_views
 
 class_name = "State"
 
+
 @app_views.route("/states", strict_slashes=False)
 def fetch_all_states():
     """Returns all states"""
     db_response = storage.all(class_name)
-    all_states = [ state.to_dict() for state in db_response.values()]
+    all_states = [state.to_dict() for state in db_response.values()]
     return all_states
 
 
@@ -23,15 +24,14 @@ def create_state():
     req_data = request.get_json()
     if not req_data:
         return abort(400, "Not a JSON")
-    
+
     state_name = req_data.get("name")
     if not state_name:
         return abort(400, "Missing name")
-    
+
     new_state = State({"name": state_name})
     new_state.save()
     return new_state.to_dict(), 201
-    
 
 
 @app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"])
@@ -44,7 +44,9 @@ def fetch_state(state_id):
     return res.to_dict()
 
 
-@app_views.route("/states/<state_id>", strict_slashes=False, methods=["DELETE"])
+@app_views.route("/states/<state_id>",
+                 strict_slashes=False,
+                 methods=["DELETE"])
 def delete_state(state_id):
     """deletes data for a single state"""
     obj = storage.get(State, state_id)
@@ -66,7 +68,7 @@ def update_state(state_id):
     req_data = request.get_json()
     if not req_data:
         return abort(400, "Not a JSON")
-    
+
     for key, val in req_data.items():
         if key not in ["id", "created_at", "updated_at"]:
             setattr(obj, key, val)
